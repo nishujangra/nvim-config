8911096bbf48b77720af71b5df4bcb3c54f5bfad
nishujangra <ndjangra1027@gmail.com>
2026-07-23 23:55:46 +0530

    style: use rose-pine-moon dark variant

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,7 +6,7 @@ return {
     	name = "rose-pine",
     	priority = 1000,
     	config = function()
-      		vim.cmd.colorscheme("rose-pine")
+      		vim.cmd.colorscheme("rose-pine-moon")
       		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     	end,
