7500b09bb6984cb6011cc75c8b124315b4374fe6
nishujangra <ndjangra1027@gmail.com>
2026-07-23 21:08:14 +0530

    feat: telescope plugin added

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+-- Written by Nishant ndjangra1027@gmail.com nishujangra.dev
+
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+}
